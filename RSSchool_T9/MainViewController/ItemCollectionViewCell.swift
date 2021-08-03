552291015531c56d7dd37d929efbff788633f851
Lazzatios <lseiilova@nu.edu.kz>
2021-08-03 18:07:34 +0600

    feat: enable autolayout of collection view cells

--- RSSchool_T9/MainViewController/ItemCollectionViewCell.swift
+++ RSSchool_T9/MainViewController/ItemCollectionViewCell.swift
@@ -16,18 +16,21 @@ class ItemCollectionViewCell: UICollectionViewCell {
         let coverImage = UIImageView()
         coverImage.contentMode = .scaleAspectFill
         coverImage.clipsToBounds = true
+        coverImage.translatesAutoresizingMaskIntoConstraints = false
         return coverImage
     }()
     
     private let coverTitleView: UILabel = {
         let coverTitle = UILabel()
         coverTitle.textColor = .white
+        coverTitle.translatesAutoresizingMaskIntoConstraints = false
         return coverTitle
     }()
     
     private let coverTypeView: UILabel = {
         let coverType = UILabel()
         coverType.textColor = .white
+        coverType.translatesAutoresizingMaskIntoConstraints = false
         return coverType
     }()
     
@@ -45,15 +48,37 @@ class ItemCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        coverImageView.frame = contentView.bounds
-        coverTitleView.frame = contentView.bounds
-        coverTypeView.frame = contentView.bounds
+        setupCoverImageLayout()
+        setupTitleLayout()
+        setupTypeLayout()
     }
     
     public func configure(with coverImage: UIImage, coverTitle: String, coverType: String) {
         coverImageView.image = coverImage
         coverTitleView.text = coverTitle
         coverTypeView.text = coverType
+    }
+    
+    private func setupCoverImageLayout() {
+        coverImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        coverImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        
+        coverImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        coverImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+        coverImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 8).isActive = true
+        coverImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -8).isActive = true
+        
+        coverImageView.widthAnchor.constraint(equalToConstant: contentView.frame.size.width).isActive = true
+        coverImageView.heightAnchor.constraint(equalToConstant: contentView.frame.size.height).isActive = true
+    }
+    
+    private func setupTitleLayout() {
+        coverTitleView.bottomAnchor.constraint(equalTo: coverImageView.bottomAnchor, constant: -30).isActive = true
+        coverTitleView.leftAnchor.constraint(equalTo: coverImageView.leftAnchor, constant: 10).isActive = true
+    }
+    
+    private func setupTypeLayout() {
+        coverTypeView.topAnchor.constraint(equalTo: coverTitleView.bottomAnchor, constant: 3).isActive = true
+        coverTypeView.leftAnchor.constraint(equalTo: coverImageView.leftAnchor, constant: 10).isActive = true
     }
 }
