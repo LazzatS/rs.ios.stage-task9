f7ab131a7a89738ce977b666707efdcf3004a07b
Lazzatios <lseiilova@nu.edu.kz>
2021-08-09 20:11:34 +0600

    feat: add gallery collection view

---
+++ RSSchool_T9/GalleryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+// 📰 🐸 
+// Project: RSSchool_T9
+// 
+// Author: Lazzat Seiilova
+// On: 09.08.2021
+// 
+// Copyright © 2021 RSSchool. All rights reserved.
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+}
