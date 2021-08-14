f73ec9a4f20bd416a7c635c6f75393ed6754a5e4
Lazzatios <lseiilova@nu.edu.kz>
2021-08-14 21:07:02 +0600

    fix: add layout for portrait/landscape orientation

--- RSSchool_T9/ItemViewController/Gallery/GalleryImageViewController.swift
+++ RSSchool_T9/ItemViewController/Gallery/GalleryImageViewController.swift
@@ -24,13 +24,15 @@ class GalleryImageViewController: UIViewController, UIScrollViewDelegate {
         let imageToShow = UIImageView()
         imageToShow.contentMode = .scaleAspectFit
         imageToShow.clipsToBounds = true
-        
+        imageToShow.translatesAutoresizingMaskIntoConstraints = false
         return imageToShow
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        galleryImageScrollView.backgroundColor = .yellow
+        galleryImageContentView.backgroundColor = .cyan
         setupScrollView()
         showImage()
     }
@@ -44,34 +46,28 @@ class GalleryImageViewController: UIViewController, UIScrollViewDelegate {
         galleryImageScrollView.addSubview(galleryImageContentView)
         
         galleryImageScrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        galleryImageScrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         galleryImageScrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         galleryImageScrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         galleryImageScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
         galleryImageContentView.centerXAnchor.constraint(equalTo: galleryImageScrollView.centerXAnchor).isActive = true
+        galleryImageContentView.centerYAnchor.constraint(equalTo: galleryImageScrollView.centerYAnchor).isActive = true
         galleryImageContentView.widthAnchor.constraint(equalTo: galleryImageScrollView.widthAnchor).isActive = true
         galleryImageContentView.topAnchor.constraint(equalTo: galleryImageScrollView.topAnchor).isActive = true
         galleryImageContentView.bottomAnchor.constraint(equalTo: galleryImageScrollView.bottomAnchor).isActive = true
     }
     
-
-    
     func showImage() {
         imageToShow.image = imageNeeded
-
-        let imageWidth = view.frame.size.width
-        let imageHeight = view.frame.size.height
-        let scaleAspect = imageHeight / imageWidth
-        
         galleryImageContentView.addSubview(imageToShow)
-        
-        imageToShow.translatesAutoresizingMaskIntoConstraints = false
-        imageToShow.leadingAnchor.constraint(equalTo: galleryImageContentView.leadingAnchor).isActive = true
-        imageToShow.trailingAnchor.constraint(equalTo: galleryImageContentView.trailingAnchor).isActive = true
+        createPortraitImageLayout()
+    }
+    
+    func createPortraitImageLayout() {
         imageToShow.topAnchor.constraint(equalTo: galleryImageContentView.topAnchor).isActive = true
         imageToShow.bottomAnchor.constraint(equalTo: galleryImageContentView.bottomAnchor).isActive = true
-        imageToShow.widthAnchor.constraint(equalToConstant: imageWidth).isActive = true
-        imageToShow.heightAnchor.constraint(equalTo: imageToShow.widthAnchor, multiplier: scaleAspect).isActive = true
+        imageToShow.leadingAnchor.constraint(equalTo: galleryImageContentView.leadingAnchor).isActive = true
         imageToShow.centerXAnchor.constraint(equalTo: galleryImageContentView.centerXAnchor).isActive = true
         imageToShow.centerYAnchor.constraint(equalTo: galleryImageContentView.centerYAnchor).isActive = true
     }
