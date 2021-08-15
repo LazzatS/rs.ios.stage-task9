4f2f41fb11958c32056da5802df954d768091b28
Lazzatios <lseiilova@nu.edu.kz>
2021-08-15 11:45:09 +0600

    feat: create objective-C files for settings

---
+++ RSSchool_T9/RSSchool_T9-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+// 📰 🐸 
+// Project: RSSchool_T9
+// 
+// Author: Lazzat Seiilova
+// On: 15.08.2021
+// 
+// Copyright © 2021 RSSchool. All rights reserved.
+
+#import "SettingsViewController.h"
+
+#ifndef RSSchool_T9_Bridging_Header_h
+#define RSSchool_T9_Bridging_Header_h
+
+
+#endif /* RSSchool_T9_Bridging_Header_h */
---
+++ RSSchool_T9/SettingsViewController/SettingsViewController.m
@@ -0,0 +1,23 @@
+//
+// 📰 🐸 
+// Project: RSSchool_T9
+// 
+// Author: Lazzat Seiilova
+// On: 15.08.2021
+// 
+// Copyright © 2021 RSSchool. All rights reserved.
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
