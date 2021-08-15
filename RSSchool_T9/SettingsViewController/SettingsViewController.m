d941bcb083fcc6b675612e888e5c9e758742e1c7
Lazzatios <lseiilova@nu.edu.kz>
2021-08-15 21:40:38 +0600

    feat: add layout constraints for settings table view

--- RSSchool_T9/SettingsViewController/SettingsViewController.m
+++ RSSchool_T9/SettingsViewController/SettingsViewController.m
@@ -24,16 +24,20 @@
 }
 
 - (void) createSettingsTableView {
-    self.settingsTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 200, 200) style:UITableViewStylePlain];
+    self.settingsTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 374, 104) style:UITableViewStylePlain];
+    self.settingsTableView.layer.cornerRadius = 16;
+    self.settingsTableView.layer.masksToBounds = YES;
+    self.settingsTableView.scrollEnabled = NO;
+    
     self.settingsTableView.translatesAutoresizingMaskIntoConstraints = false;
     self.settingsTableView.delegate = self;
     self.settingsTableView.dataSource = self;
     [self.view addSubview:self.settingsTableView];
     
-    [self.settingsTableView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:34].active = true;
-    [self.settingsTableView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-200].active = true;
-    [self.settingsTableView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:20].active = true;
-    [self.settingsTableView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:-20].active = true;
+    [self.settingsTableView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = true;
+    [self.settingsTableView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:123].active = true;
+    [self.settingsTableView.widthAnchor constraintEqualToConstant:374].active = true;
+    [self.settingsTableView.heightAnchor constraintEqualToConstant:104].active = true;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -45,6 +49,7 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     }
+    [cell.heightAnchor constraintEqualToConstant: (self.settingsTableView.frame.size.height / 2)].active = true;
     cell.textLabel.text = @"blabla";
     return cell;
 }
